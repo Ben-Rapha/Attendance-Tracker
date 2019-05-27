308054f242a5319876c6b25b4282ca71c252c8eb
Ben-Rapha <sultan.mcmxcii@gmail.com>
2019-05-27 12:55:28 -0400

    created the xml layout for the app's login

--- app/src/main/java/com/example/attendancetracker/UI/LoginActivity.java
+++ app/src/main/java/com/example/attendancetracker/UI/LoginActivity.java
@@ -7,14 +7,25 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.attendancetracker.R;
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
+
+    MaterialButton mSignInButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_activity);
-        removeStatusBar();
+        setContentView(R.layout.authenticate_password_activity);
+        mSignInButton = findViewById(R.id.signInButton);
+
+//        mSignInButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+//        removeStatusBar();
     }
 
     private void removeStatusBar() {
