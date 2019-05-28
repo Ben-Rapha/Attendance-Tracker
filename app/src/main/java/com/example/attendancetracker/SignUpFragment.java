b9773a7f315c75242c606c09a16f7b2e1e10ada7
Ben-Rapha <sultan.mcmxcii@gmail.com>
2019-05-28 06:15:54 -0400

    added navigation component to login screen

---
+++ app/src/main/java/com/example/attendancetracker/SignUpFragment.java
@@ -0,0 +1,32 @@
+package com.example.attendancetracker;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+public class SignUpFragment extends Fragment {
+
+
+    public SignUpFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.sign_up_fragment,
+                container, false);
+    }
+
+
+}
--- app/src/main/java/com/example/attendancetracker/UI/LoginActivity.java
+++ app/src/main/java/com/example/attendancetracker/UI/LoginActivity.java
@@ -1,36 +1,40 @@
 package com.example.attendancetracker.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.attendancetracker.Login_Fragment;
 import com.example.attendancetracker.R;
 import com.google.android.material.button.MaterialButton;
 
-public class LoginActivity extends AppCompatActivity {
+
+
+
+public class LoginActivity extends AppCompatActivity implements
+        Login_Fragment.onButtonClickListener {
 
     MaterialButton mSignInButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.authenticate_password_activity);
+        setContentView(R.layout.login_host);
         mSignInButton = findViewById(R.id.signInButton);
-
-//        mSignInButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//            }
-//        });
-//        removeStatusBar();
     }
 
     private void removeStatusBar() {
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
+    }
+
+    @Override
+    public void goToDestination() {
+      androidx.navigation.Navigation.findNavController(this,R.id.login_nav_host)
+              .navigate(R.id.action_main_destination_to_signUpFragment);
     }
 }
